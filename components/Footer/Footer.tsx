b1af44ffbbd7cfad1183e7d08f3e18789bf31c0a
Anastasia199129 <nastasia199126@gmail.com>
2023-11-22 01:09:34 +0100

    added footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+
+import data from '@/public/data/footer.json'
+
+import s from './Footer.module.sass'
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={s.container}>
+      {data?.map(({ id, link, icon }) => (
+        <Link className={s.link} key={id} href={link}>
+          <img 
+            src={icon.url} 
+            alt={icon.alt} 
+            title={icon.title} 
+          />
+        </Link>
+      ))}
+    </div>
   )
 }
